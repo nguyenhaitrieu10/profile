dad1bd545e6006c6620ef9d42d317962c049adc1
Hai-Trieu <trieu.nh@teko.vn>
2019-05-30 14:12:18 +0700

    Updates

---
+++ precache-manifest.423fcdcfb27092ea08e89b79abdcd4bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36b54e79dc81d110ce04d114382c7c3",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "ad4bd87950738ab0f3f1",
+    "url": "/profile/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/profile/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ad4bd87950738ab0f3f1",
+    "url": "/profile/static/js/main.562e972f.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  }
+]);
